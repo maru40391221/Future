09b4d4edcf4b6a30b8f78078548e98c29f83d546
maru40391221 <polorll.1221@gmail.com>
2023-02-18 22:44:48 +0900

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveAmount;
+    private Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+
+        anim = GetComponent<Animator> ();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed; 
+
+
+        if (moveInput != Vector2.zero)
+    { 
+        anim.SetBool("isRunning", true);
+    }
+    else 
+    { 
+        anim.SetBool("isRunning", false);
+    }
+    
+
+    }
+
+        private void FixedUpdate() {
+        rb.MovePosition(rb.position + moveAmount *Time.fixedDeltaTime);
+    }
+
+
+
+    
+}
