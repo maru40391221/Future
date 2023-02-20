1064c5ddddd2939a5de08201742229cfc71d2785
maru40391221 <polorll.1221@gmail.com>
2023-02-20 18:39:15 +0900

    Projectiles move forward (to the left side on the screen)

---
+++ Assets/projectile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+     transform.Translate(Vector2.left *speed*Time.deltaTime);
+    }
+}
