5a9e3343a9faf278e25b3d62d7e0f8a56c6f7a28
maru40391221 <polorll.1221@gmail.com>
2023-02-21 19:44:32 +0900

    Update on Weapon Script
    Added rotation but may deleate later

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -18,6 +18,12 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle + 180, Vector3.forward);
+        transform.rotation = rotation;
+
+
         if (Input.GetMouseButton(0)){
             if (Time.time >= shotTime){
                 Instantiate(projectile, shotPoint.position, transform.rotation);
