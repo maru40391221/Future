de760ac9099d1d34541af7183a2909132d40069a
maru40391221 <polorll.1221@gmail.com>
2023-02-24 17:58:59 +0900

    Programmed Jumping action

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public float jumpForce;
     private float moveInput;
     private Rigidbody2D rb;
     private bool facingRight = true;
-
     private bool isGrounded; 
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;  
-
-    public int extraJumps;
+    public float jumpForce;
+    private int extraJumps;
+    public int extraJumpsValue;
 
 
     private void Start() {
+        extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck, checkRadius, whatIsGround);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput*speed, rb.velocity.y);
@@ -43,6 +43,23 @@ public class PlayerController : MonoBehaviour
         }
 
 
+    }
+
+    private void Update() {
+
+        if (isGrounded == true)
+        {
+            extraJumps=extraJumpsValue;
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps>0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
     }
 
     void Flip (){
