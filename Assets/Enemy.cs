945cf8c8025c607a0b602c309eae0dc9086dbecd
maru40391221 <polorll.1221@gmail.com>
2023-03-12 20:05:03 +0900

    trying to program enemies to leap towards the player

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -11,6 +11,9 @@ public class Enemy : MonoBehaviour
     public Transform player;
     public float stopDistance;
     public float speed;
+    private float attackTime;
+    public float timeBetweenAttacks;
+    public float attackSpeed;
 
 
     private void Start() 
@@ -20,12 +23,37 @@ public class Enemy : MonoBehaviour
 
     private void Update() 
     {
-        if (player != null){
+        if (player != null)
+        {
             if (Vector2.Distance(transform.position, player.position) > stopDistance){
                 transform.position = Vector2.MoveTowards(transform.position, player.position, speed*Time.deltaTime);
+            }else 
+            {
+                if (Time.time >=attackTime){
+                     StartCoroutine(Attack());
+                    attackTime = Time.time + timeBetweenAttacks;
+                     }
+                
             }
         }
-    }
+        
+        IEnumerator Attack() 
+        {
+            player.GetComponent<Player>().TakeDamage(damage);
+
+            Vector2 originalPosition = transform.position;
+            Vector2 targetPosition = player.position;
+            float percent = 0;
+            while (percent<= 1)
+            {
+                percent += Time.deltaTime * attackSpeed;
+                float formula = (-Mathf.Pow (percent, 2) + percent) * 4;
+                transform.position = Vector2.Lerp (originalPosition, targetPosition, formula);
+                yield return null;
+            }
+        }
+
+        
    
    public void TakeDamage (int damageAmount) 
    {
@@ -34,6 +62,5 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
     }
    }
-
 
 }
