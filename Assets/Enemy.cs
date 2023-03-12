45ae6c6c93b26095710e299f61637a7604ac4099
maru40391221 <polorll.1221@gmail.com>
2023-03-12 19:46:01 +0900

    enemy with an ability to follow gameobjects with the player taga and stop at the certain distance

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,11 +6,34 @@ public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public int health;
+
+    [HideInInspector]
+    public Transform player;
+    public float stopDistance;
+    public float speed;
+
+
+    private void Start() 
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update() 
+    {
+        if (player != null){
+            if (Vector2.Distance(transform.position, player.position) > stopDistance){
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed*Time.deltaTime);
+            }
+        }
+    }
    
-   public void TakeDamage (int damageAmount) {
+   public void TakeDamage (int damageAmount) 
+   {
     health -= damageAmount;
     if (health<=0) {
         Destroy(gameObject);
     }
    }
+
+
 }
