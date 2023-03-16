718e907128fe98b801475a883b872838820a5ae9
maru40391221 <polorll.1221@gmail.com>
2023-03-16 18:52:26 +0900

    The spawner is able to repeatedly instantiate the enemy but the spawner does not take hits

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -7,12 +7,6 @@ public class Spawner : MonoBehaviour
     public int health;
     [HideInInspector]
     public Transform player;
-    public float speed;
-    public float timeBetweenAttacks;
-    public int damage;
-    public float minX;
-    public float maxX;
-    private Vector2 targetPosition;
     public float timeBetweenSummons;
     private float summonTime;
     public GameObject enemyToSummon;
@@ -21,18 +15,10 @@ public class Spawner : MonoBehaviour
     private void Start() 
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        float randomX = Random.Range(minX,maxX);
-        targetPosition = new Vector2 (randomX, transform.position.y);
     }
 
     private void Update() {
         if (player != null)
-        {
-            if (Vector2.Distance (transform.position, targetPosition) > .5f)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-            }
-        } else 
         {
             if (Time.time >= summonTime)
             {
@@ -43,7 +29,6 @@ public class Spawner : MonoBehaviour
 
         }
     }
-
 
     public void TakeDamage (int damageAmount) 
    {
