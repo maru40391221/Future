993858d64a39b186d404f26e989a12ef7bc23b36
maru40391221 <polorll.1221@gmail.com>
2023-04-10 20:47:12 +0900

    removed Spawner's random moving

--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 
 public class spawner : generalEnemy
 {
-    public float minX;
-    public float maxX;
-    private Vector2 targetPosition;
-
     public float timeBetweenSummons;
     private float summonTime;
     public GameObject enemyToSummon;
     public override void Start()
     {
         base.Start();
-        float randomX = Random.Range(minX,maxX);
-        float currentY = transform.position.y;
-        targetPosition = new Vector2 (randomX, transform.position.y);
     }
 
     // Update is called once per frame
@@ -24,18 +17,12 @@ public class spawner : generalEnemy
     {
         if (player != null)
         {
-            if (Vector2.Distance (transform.position, targetPosition) > .5f)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-                
-            } else
-            {
                 if (Time.time >= summonTime)
                 {
                     summonTime = Time.time + timeBetweenSummons;
                     summon();
                 }
-            }
+            
         }
     }
     public void summon ()
