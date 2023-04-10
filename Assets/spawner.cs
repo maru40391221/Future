3a470e811da99d87628741b455a14ce0c7292886
maru40391221 <polorll.1221@gmail.com>
2023-04-10 22:29:31 +0900

    fixed spawner's moving

--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class spawner : generalEnemy
 {
+ //summoning
     public float timeBetweenSummons;
     private float summonTime;
     public GameObject enemyToSummon;
+ //summoning
+
+ //moving
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+    private float waitTime;
+    public float startWaitTime;
+
+//moving
     public override void Start()
     {
         base.Start();
+
+        randomSpot = Random.Range(0, moveSpots.Length);
+        waitTime = startWaitTime;
     }
 
     // Update is called once per frame
@@ -17,13 +31,31 @@ public class spawner : generalEnemy
     {
         if (player != null)
         {
-                if (Time.time >= summonTime)
-                {
+       transform.position = Vector2.MoveTowards (transform.position, moveSpots[randomSpot].position, speed*Time.deltaTime);
+//summoning
+        if (Time.time >= summonTime)
+        {
                     summonTime = Time.time + timeBetweenSummons;
                     summon();
-                }
+        }
+ //summoning
             
         }
+ //moving
+ 
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f )
+        {
+            if (waitTime <= 0){
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }else{
+            waitTime -= Time.deltaTime;
+            }
+
+        }
+ //moving
+
+
     }
     public void summon ()
     {
